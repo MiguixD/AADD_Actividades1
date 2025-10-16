c4becbe607d628d99fc5150aab57e9e88a5e55b3
Miguel Martin Madrid <pokemon2005mmm@gmail.com>
2025-10-16 19:23:25 +0200

    Ejercicio1_4 terminado (con el error de que sobreescribe en el archivo)

--- src/main/java/es/cifpcarlos3/actividad1_4/GestorFormacion2.java
+++ src/main/java/es/cifpcarlos3/actividad1_4/GestorFormacion2.java
@@ -1,13 +1,11 @@
 package es.cifpcarlos3.actividad1_4;
 
-import es.cifpcarlos3.actividad1_3.vo.Ciclo;
+import es.cifpcarlos3.actividad1_4.vo.Ciclo;
 import es.cifpcarlos3.actividad1_4.vo.FamiliaProfesional;
 import es.cifpcarlos3.actividad1_4.vo.Grado;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,15 +17,33 @@ public class GestorFormacion2 {
                 "/grados.csv");
         File pathCiclo = new File("./src/main/java/es/cifpcarlos3/actividad1_4" +
                 "/informacion_ciclos.txt");
+        File pathListaSerializada = new File("./src/main/java/es/cifpcarlos3/actividad1_4" +
+                "/lista_ciclos.ser");
         String codFP = args[0];
         String codGrado = args[1];
 
         FamiliaProfesional familiaProfesional = getFamiliaProfesional(pathFP, codFP);
         Grado grado = getGrado(pathGrado, codGrado);
 
+        List<Ciclo> ciclos = getCiclos(pathCiclo, familiaProfesional, grado);
+
+        System.out.println("Familia profesional: " + familiaProfesional.getNombre() + "\n");
+        escribirEnArchivoBinario(pathListaSerializada, "Familia profesional: " + familiaProfesional.getNombre() + "\n");
+        System.out.println("Grado seleccionado: " +  grado.getNombre() + "\n");
+        escribirEnArchivoBinario(pathListaSerializada, "Grado seleccionado: " +  grado.getNombre() + "\n");
+        System.out.println("Ciclos encontrados:\n");
+        escribirEnArchivoBinario(pathListaSerializada, "Ciclos encontrados:\n");
+
+        for(Ciclo ciclo: ciclos) {
+            System.out.println("El ciclo " + ciclo.getDescripcion() + " en la familia de " +
+                    familiaProfesional.getNombre() + " es un " + grado.getNombre() + ".");
+            escribirEnArchivoBinario(pathListaSerializada, "El ciclo " + ciclo.getDescripcion() + " en la familia de " +
+                    familiaProfesional.getNombre() + " es un " + grado.getNombre() + ".");
+        }
+
     }
     public static FamiliaProfesional getFamiliaProfesional(File path, String codigo) {
-        FamiliaProfesional familiaProfesional = null;
+        FamiliaProfesional familiaProfesional = new FamiliaProfesional();
         if(path.exists()) {
             try(FileReader fr = new FileReader(path);
                 BufferedReader br = new BufferedReader(fr)) {
@@ -50,7 +66,7 @@ public class GestorFormacion2 {
     }
 
     public static Grado getGrado(File path, String codigo) {
-        Grado grado = null;
+        Grado grado = new Grado();
         if(path.exists()) {
             try(FileReader fr = new FileReader(path);
                 BufferedReader br = new BufferedReader(fr)) {
@@ -85,7 +101,8 @@ public class GestorFormacion2 {
                         partes[i] = validarCadena(partes[i]);
                     }
                     if(partes[3].equals(familiaProfesional.getCodigo()) && partes[4].equals(grado.getCodigo())) {
-                        Ciclo ciclo = new Ciclo
+                        Ciclo ciclo = new Ciclo(partes[0], partes[1], Integer.parseInt(partes[2]), partes[3], partes[4]);
+                        ciclos.add(ciclo);
                     }
                 }
             } catch (IOException ex) {
@@ -95,11 +112,29 @@ public class GestorFormacion2 {
         return ciclos;
     }
 
+    public static void escribirEnArchivoBinario(File path, String texto) {
+        if(!path.exists()) {
+            try {
+                path.createNewFile();
+            } catch (IOException ex) {
+                System.out.println("Error al crear el archivo");
+            }
+        }
+        try(FileOutputStream fos = new FileOutputStream(path);
+            BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+            byte[] bytes = texto.getBytes(StandardCharsets.UTF_8);
+            bos.write(bytes);
+        } catch (IOException ex) {
+            System.out.println("Error al escribir archivo");
+        }
+    }
+
     public static String validarCadena(String cadena) {
         cadena = cadena.trim();
         if(cadena.startsWith("'")) {
             cadena = cadena.substring(1);
-        } else if(cadena.endsWith("'")) {
+        }
+        if(cadena.endsWith("'")) {
             cadena = cadena.substring(0, cadena.length()-1);
         }
         return cadena;
